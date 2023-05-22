b6bcb545494191001c7d376b445c9e8242b83ff0
Ditul-Roy <ditulr@gmail.com>
2023-05-22 09:51:57 +0600

    chack

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('running')
+})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://toyServer:fK80gLoQLCyp9J16@cluster0.bylwpc6.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const chackCollection = client.db('chackDB').collection('chack');
+
+    app.get('/c', async(req, res) => {
+        const result = await chackCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, ()=>{
+    console.log(`port is: ${port}`);
+})
